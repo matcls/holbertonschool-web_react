eb90c8adbbac18a326012eb6aff783fe184b69fb
Manuel Torres <manueltorresvesga@gmail.com>
2022-02-19 15:11:54 -0500

    typo

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseShape.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseShape.js
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const CourseShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
-  name: PropTypes.string..isRequired,
-  credit: PropTypes.number..isRequired,
+  name: PropTypes.string.isRequired,
+  credit: PropTypes.number.isRequired,
 });
 
 export default CourseShape;

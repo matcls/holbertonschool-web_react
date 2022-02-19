ebd90058d890d6d40bbf43f555c813bc6e458c31
Manuel Torres <manueltorresvesga@gmail.com>
2022-02-19 14:57:45 -0500

    Correct test for App component

--- 0x03-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.test.js
@@ -15,19 +15,4 @@ describe('Test the <App /> Component', () => {
   it('<App /> renders without crashing', () => {
     expect(wrapper.exists()).toEqual(true);
   });
-  it("<App /> contains the <Notifications /> Component", () => {
-    expect(wrapper.contains(<Notifications />)).toEqual(true);
-  });
-
-  it("<App /> contains the <Header /> Component", () => {
-    expect(wrapper.contains(<Header />)).toEqual(true);
-  });
-
-  it("<App /> contains the <Login /> Component", () => {
-    expect(wrapper.contains(<Login />)).toEqual(true);
-  });
-
-  it("<App /> contains the <Footer /> Component", () => {
-    expect(wrapper.contains(<Footer />)).toEqual(true);
-  });
 });

d50e1cd9a4ace2bfd4e158be66c359ab66ced1a5
Manuel Torres <manueltorresvesga@gmail.com>
2022-02-19 14:38:16 -0500

    Correct filenames of the tests in CourseList, which were reversed.

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseLIst.test.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseLIst.test.js
@@ -1,49 +1,36 @@
 import React from "react";
 import { shallow } from "enzyme";
-import CourseListRow from "./CourseListRow";
+import CourseList from "./CourseList";
 
-describe("Test <CourseListRow />", () => {
-  it("<CourseListRow /> renders without crashing", () => {
-    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+describe("Test <CourseList />", () => {
+  it("<CourseList /> renders without crashing", () => {
+    const wrapper = shallow(<CourseList />);
     expect(wrapper.exists());
   });
-  it("Renders one cell with colspan = 2 when textSecondCell does not exist", () => {
-    const wrapper = shallow(
-      <CourseListRow isHeader={true} textFirstCell="test" />
-    );
+  it("Renders the 5 different rows", () => {
+    const wrapper = shallow(<CourseList />);
     wrapper.update();
-    const item = wrapper.find("th");
+    const item = wrapper.find("CourseListRow");
 
-    expect(item).toHaveLength(1);
-    expect(item.prop("colSpan")).toEqual("2");
-  });
-  it("Renders two cells when textSecondCell is present", () => {
-    const wrapper = shallow(
-      <CourseListRow
-        isHeader={true}
-        textFirstCell="one"
-        textSecondCell="two"
-      />
-    );
-    wrapper.update();
-    const item = wrapper.find("th");
+    expect(item).toHaveLength(5);
 
-    expect(item).toHaveLength(2);
-    expect(item.first().text()).toEqual("one");
-    expect(item.at(1).text()).toEqual("two");
-  });
-  it("Renders correctly two td elements within a tr element", () => {
-    const wrapper = shallow(
-      <CourseListRow
-        isHeader={false}
-        textFirstCell="one"
-        textSecondCell="two"
-      />
-    );
-    wrapper.update();
-    const item = wrapper.find("tr");
+    expect(item.at(0).prop("textFirstCell")).toEqual("Available courses");
+    expect(item.at(0).prop("isHeader")).toEqual(true);
 
-    expect(item).toHaveLength(1);
-    expect(item.children("td")).toHaveLength(2);
+    expect(item.at(1).prop("textFirstCell")).toEqual("Course name");
+    expect(item.at(1).prop("textSecondCell")).toEqual("Credit");
+    expect(item.at(1).prop("isHeader")).toEqual(true);
+
+    expect(item.at(2).prop("textFirstCell")).toEqual("ES6");
+    expect(item.at(2).prop("textSecondCell")).toEqual("60");
+    expect(item.at(2).prop("isHeader")).toEqual(false);
+
+    expect(item.at(3).prop("textFirstCell")).toEqual("Webpack");
+    expect(item.at(3).prop("textSecondCell")).toEqual("20");
+    expect(item.at(3).prop("isHeader")).toEqual(false);
+
+    expect(item.at(4).prop("textFirstCell")).toEqual("React");
+    expect(item.at(4).prop("textSecondCell")).toEqual("40");
+    expect(item.at(4).prop("isHeader")).toEqual(false);
   });
 });
